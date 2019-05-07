304e205b7d151439aafcce8621830377acd913b6
JuanNotario <juan.notario20@gmail.com>
2019-05-07 14:01:32 +0200

    Botones añadidos

--- app/src/main/java/com/companbook/MainActivity.java
+++ app/src/main/java/com/companbook/MainActivity.java
@@ -2,12 +2,21 @@ package com.companbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button logIn;
+    Button registrarse;
+    Button accesoUsr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logIn.findViewById(R.id.btnLogIn);
+        registrarse.findViewById(R.id.btnRegistrarse);
+        accesoUsr.findViewById(R.id.btnAccesoUsr);
     }
 }
